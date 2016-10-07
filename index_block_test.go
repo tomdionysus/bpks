d47fbd47c868183b310c9402cd6363707fc2c5e9
Tom Cully <tom.cully@apitalent.co.nz>
2016-10-07 19:40:07 +1300

    More IndexBlock tests

--- index_block_test.go
+++ index_block_test.go
@@ -36,18 +36,18 @@ func TestIndexBlockAdd(t *testing.T) {
 	assert.Equal(t, k.Key, (*kvs.Root.KeyPointerList)[1].Key)
 	assert.Equal(t, k.BlockAddress, (*kvs.Root.KeyPointerList)[1].BlockAddress)
 
-	// k3 := KeyPointer{
-	// 	Key:          Key{1, 1, 1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	// 	BlockAddress: 81723123,
-	// }
-	// err = kvs.Root.Add(k)
-	// assert.Nil(t, err)
+	k3 := KeyPointer{
+		Key:          Key{1, 1, 1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		BlockAddress: 81723123,
+	}
+	err = kvs.Root.Add(k3)
+	assert.Nil(t, err)
 
-	// assert.Equal(t, 3, len(*kvs.Root.KeyPointerList))
-	// assert.Equal(t, k2.Key, (*kvs.Root.KeyPointerList)[0].Key)
-	// assert.Equal(t, k2.BlockAddress, (*kvs.Root.KeyPointerList)[0].BlockAddress)
-	// assert.Equal(t, k.Key, (*kvs.Root.KeyPointerList)[1].Key)
-	// assert.Equal(t, k.BlockAddress, (*kvs.Root.KeyPointerList)[1].BlockAddress)
-	// assert.Equal(t, k3.Key, (*kvs.Root.KeyPointerList)[2].Key)
-	// assert.Equal(t, k3.BlockAddress, (*kvs.Root.KeyPointerList)[2].BlockAddress)
+	assert.Equal(t, 3, len(*kvs.Root.KeyPointerList))
+	assert.Equal(t, k2.Key, (*kvs.Root.KeyPointerList)[0].Key)
+	assert.Equal(t, k2.BlockAddress, (*kvs.Root.KeyPointerList)[0].BlockAddress)
+	assert.Equal(t, k.Key, (*kvs.Root.KeyPointerList)[1].Key)
+	assert.Equal(t, k.BlockAddress, (*kvs.Root.KeyPointerList)[1].BlockAddress)
+	assert.Equal(t, k3.Key, (*kvs.Root.KeyPointerList)[2].Key)
+	assert.Equal(t, k3.BlockAddress, (*kvs.Root.KeyPointerList)[2].BlockAddress)
 }
